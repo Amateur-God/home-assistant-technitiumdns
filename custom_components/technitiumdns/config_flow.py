d739cbfb7ef6587f4be47efc0eab94f350ce3e14
Amateur-God <mrhowe95@gmail.com>
2025-07-23 16:47:56 +0100

    possible update for future deprecation in HASSE code

--- custom_components/technitiumdns/config_flow.py
+++ custom_components/technitiumdns/config_flow.py
@@ -100,10 +100,6 @@ class TechnitiumDNSConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 class TechnitiumDNSOptionsFlowHandler(config_entries.OptionsFlow):
     """Handle an options flow for TechnitiumDNS."""
 
-    def __init__(self, config_entry: config_entries.ConfigEntry):
-        """Initialize options flow handler."""
-        self.config_entry = config_entry
-
     async def async_step_init(self, user_input=None):
         """Manage the options."""
         if user_input is not None:
